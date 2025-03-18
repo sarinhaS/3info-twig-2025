71ffddc86f7b03042b10d1b7b8c28adf833c2698
Sarinha 3 <125043002+sarinhaS@users.noreply.github.com>
2025-03-18 08:47:24 -0300

    Terceiro commit

--- compras.php
+++ compras.php
@@ -2,6 +2,14 @@
 // compras.php
 require_once('twig_carregar.php');
 
+require('inc/banco.php');
+
+// busca as compras no banco    
+$dados = $pdo->query('SELECT * FROM compras');
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+
 echo $twig->render('compras.html', [
     'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
